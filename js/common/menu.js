e1ec4c8fa71b089a8b82cdb55c97e976ddfae85e
chanvivi <chenvivi128@gmail.com>
2025-07-06 20:17:07 +0800

    新增滾動動態

--- js/common/menu.js
+++ js/common/menu.js
@@ -5,3 +5,21 @@ toggle.addEventListener('click', () => {
   links.classList.toggle('active');
   toggle.classList.toggle('active');
 });
+
+  // <!-- 滾動動畫 Intersection Observer -->
+    const fadeIns = document.querySelectorAll('.fade-in');
+    const options = {
+      threshold: 0.1
+    };
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting){
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    fadeIns.forEach(el => {
+      observer.observe(el);
+    });
